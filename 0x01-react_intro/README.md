1d91c6af6d48eb88529078ce180321256d208368
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-07 16:59:00 +0100

    added login

--- 0x01-react_intro/README.md
+++ 0x01-react_intro/README.md
@@ -3,3 +3,6 @@ Introduction to react. Alx software engineering program.
 
 ## task_0
 Task 0: Create a simple application that displays holberton school logo.
+
+## task_1
+Task 1: Create a notification bar
---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+export default function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={require('./holberton_logo.jpg')} alt='holberton logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for='email'>Email </label>
+        <input type='email' id='email'></input>
+        <label for='password'> Password </label>
+        <input type='password' id='password'></input>
+        <button type='submit'>OK</button>
+      </body>
+      <footer className='App-footer'>Copyright {getFullYear()} - {getFooterCopy()}</footer>
+    </div>
+  )
+}
