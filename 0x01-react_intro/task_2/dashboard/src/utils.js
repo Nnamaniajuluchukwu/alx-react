6dbd55199ed64dd0f099563e75a5df5691ec38ec
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-07 18:00:30 +0100

    syle notification

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -15,3 +15,7 @@ export function getFooterCopy(isIndex) {
     if (isIndex) return 'Holberton School';
     else return 'Holberton School main dashboard';
 }
+
+export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+}
