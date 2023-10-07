75bd1b700276ca6295e663155a61aaa1db261050
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-07 19:38:29 +0100

    test for utils

---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Notifications.css';
+import { getLatestNotification } from "./utils";
+
+
+export default function Notifications() {
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
+            <button style={{
+                float: 'right',
+                marginTop: '-9.5em',
+                marginRight: '-0.5em',
+                backgroundColor: '#fff',
+            }} aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
+                <img src={require('./close-button.png')} height="15px" width="13px" alt="close icon"/>
+            </button>
+        </div>
+    );
+}
