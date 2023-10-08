00a3c09b3a63cacb9065dd4a8c2b07832d9e1914
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-08 13:34:51 +0100

    setup notifications test

--- 0x01-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import App from './App';
-import Notifications from '../Notifications/Notifications';
+
 
 
 describe('App Component', () => {
@@ -31,31 +31,6 @@ describe('App Component', () => {
 
     it('should match the snapshot', () => {
         const wrapper = shallow(<App />);
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
-});
-
-
-describe('Notifications Component', () => {
-    it('should render without crashing', () => {
-        const wrapper = shallow(<Notifications />);
-        expect(wrapper.exists()).toBeTruthy();
-    });
-
-    it('renders three list items', () => {
-        const wrapper = shallow(<Notifications />);
-        const listItems = wrapper.find('li');
-        expect(listItems.length).toEqual(3);
-    });
-
-    it("renders the text 'Here is the list of notifications'", () => {
-        const wrapper = shallow(<Notifications />);
-        const text = wrapper.find('p');
-        expect(text.props().children).toEqual('Here is the list of notifications');
-    });
-
-    it('should match the snapshot', () => {
-        const wrapper = shallow(<Notifications />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
