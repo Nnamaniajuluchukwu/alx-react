e37e45f5af1ef0b4f18f0a16120d8453cd970bcb
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-11 13:27:29 +0100

    created tests for Components, configured jest file

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notificaations.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from '../Notifications/Notifications';
+import toJson from 'enzyme-to-json';
+
+
+describe('Notifications Component', () => {
+    it('should render without crashing', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.exists()).toBeTruthy();
+    });
+
+    it('renders three list items', () => {
+        const wrapper = shallow(<Notifications />);
+        const listItems = wrapper.find('li');
+        expect(listItems.length).toEqual(3);
+    });
+
+    it("renders the text 'Here is the list of notifications'", () => {
+        const wrapper = shallow(<Notifications />);
+        const text = wrapper.find('p');
+        expect(text.props().children).toEqual('Here is the list of notifications');
+    });
+
+    it('should match the snapshot', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
