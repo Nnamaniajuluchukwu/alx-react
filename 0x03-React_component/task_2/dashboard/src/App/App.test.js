655beb4a33e4f2ccadd10eb1b3963a73267d5df7
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-25 18:55:13 +0100

    commented out mount

--- 0x03-React_component/task_2/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_2/dashboard/src/App/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+// import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import App from './App';
 import Notifications from '../Notifications/Notifications';
