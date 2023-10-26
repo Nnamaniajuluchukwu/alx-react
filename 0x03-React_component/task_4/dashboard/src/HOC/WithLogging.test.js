79fc0bcdbae7236d05d7fb53e5b6a0517478bc19
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-26 23:47:30 +0100

    added test for higher order component

---
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import WithLogging from './WithLogging';
+import BodySection from '../BodySection/BodySection';
+
+describe('WithLogging HOC', () => {
+  it('should log on mount and unmount with "BodySection"', () => {
+    const consoleLog = jest.spyOn(console, 'log');
+    const WrappedComponent = WithLogging(() => <BodySection />);
+    const wrapper = mount(<WrappedComponent />);  // Enzyme issues: Please if you have a fix, create a pull request
+    wrapper.unmount();
+    expect(consoleLog).toHaveBeenCalledWith('Component BodySection is mounted on componentDidMount()');
+    expect(consoleLog).toHaveBeenCalledWith('Component BodySection is going to unmount on componentWillUnmount()');
+    consoleLog.mockRestore();
+  });
+});
