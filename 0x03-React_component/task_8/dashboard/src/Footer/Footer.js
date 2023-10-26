e509fcd023f6a60863d5ecd18725ea7ce8d21927
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-10-26 23:33:11 +0100

    write test for HOC function

---
+++ 0x03-React_component/task_8/dashboard/src/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+
+export default function Footer() {
+    return <footer className='App-footer'>Copyright {getFullYear()} - {getFooterCopy()}</footer>;
+}
