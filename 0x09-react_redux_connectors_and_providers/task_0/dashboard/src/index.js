462ceabd5143b97fe81371d75d183db7de2df89c
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-11-21 16:23:27 +0100

    create store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import uiReducer from "./reducers/uiReducer";
+// import { configureStore } from "@reduxjs/toolkit";
+
+// Better method
+// const store1 = configureStore({
+//   reducer: {
+//     ui: uiReducer,
+//   }
+// });
+
+const store = createStore(uiReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
